5415c9b8624e2901d9d3adb22854ba91d15d7c34
milliwonkim <39076645+milliwonkim@users.noreply.github.com>
2020-08-23 23:08:19 +0900

    Update index.js

--- src/Firebase/index.js
+++ src/Firebase/index.js
@@ -3,14 +3,14 @@ import 'firebase/storage';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: 'AIzaSyAriCwAXpvqtdE2kaeDwRfrSRyQnnQOl5c',
-    authDomain: 'react-firebase-image-upl-64f10.firebaseapp.com',
-    databaseURL: 'https://react-firebase-image-upl-64f10.firebaseio.com',
-    projectId: 'react-firebase-image-upl-64f10',
-    storageBucket: 'react-firebase-image-upl-64f10.appspot.com',
-    messagingSenderId: '237901371700',
-    appId: '1:237901371700:web:9f7d0391670d239abd2d61',
-    measurementId: 'G-6Q97Z67T5H',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
